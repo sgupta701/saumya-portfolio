3ce506d85fc1738b659c86b3164af5a1952189ac
Saumya Gupta <gupta.sg003@gmail.com>
2026-06-02 20:02:07 +0530

    update resume drive-link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ const Contact = () => {
               { name: 'GITHUB', link: 'https://github.com/sgupta701' },
               { name: 'LINKEDIN', link: 'https://www.linkedin.com/in/saumya-gupta-4385452a4/' },
               { name: 'INSTAGRAM', link: 'https://www.instagram.com/__gupta.saumya_?igsh=MXNiMmh3OHJmdnpxdA==' },
-              { name: 'RESUME', link: 'https://drive.google.com/file/d/1YBIfZbE4Ypj2BZ8hCL-89H6ygSZXMN7v/view?usp=sharing' }
+              { name: 'RESUME', link: 'https://drive.google.com/file/d/1M2K1imOWHLQaUb5mbmWh21jENh5wBtL2/view?usp=sharing' }
             ].map((social) => (
               <a 
                 key={social.name} 
